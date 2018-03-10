3665d075e758cf24c10631b68cce217c6585baaa
Cella <gerardo_cella@yahoo.fr>
2018-03-10 11:15:11 +0100

    Sql
    mise a jour

--- naveDb/Tables/City.sql
+++ naveDb/Tables/City.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[City]
 (
-	[Id] INT NOT NULL IDENTITY, 
+	[Id] INT NOT NULL, 
     [PostalCode] VARCHAR(4) NOT NULL, 
     [Name] VARCHAR(50) NOT NULL, 
     CONSTRAINT [PK_City] PRIMARY KEY ([Id])
