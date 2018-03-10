acb6b6a31b74f6a3259713179ba05be22fc825db
Cella <gerardo_cella@yahoo.fr>
2018-03-10 11:40:52 +0100

    test
    test

--- Hackatheam2017/Controllers/HomeController.cs
+++ Hackatheam2017/Controllers/HomeController.cs
@@ -17,11 +17,16 @@ namespace Hackatheam2017.Controllers
             _SqlAuthConnectionString = @"Server=FLORLIFE;" +
                                        "Database=naveDb;" +
                                        "User Id=sa;Password=tftic@2012";
+            _SqlAuthConnectionString = @"Server=tcp:navetteurserver.database.windows.net,1433;Initial Catalog=navDb;Persist Security Info=False;User ID={trebizonder};Password={tftic@2018};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
             _con = new Connection(_SqlAuthConnectionString, DbProviderFactories.GetFactory("System.Data.SqlClient"));
         }
 
         public ActionResult Index()
         {
+            Command cmd = new Command("SELECT Name FROM City WHERE PostalCode=6041");
+
+            ViewBag.CP = (string)_con.ExecuteScalar(cmd);
             return View();
         }
 
