54e38bcefa78f1af1a98968aa99669a7cffcb769
Cella <gerardo_cella@yahoo.fr>
2018-03-10 18:15:16 +0100

    tiens
    ;-)

---
+++ Hackatheam2017/Models/Form/Search.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackatheam2017.Models.Form
+{
+    public class Search
+    {
+    }
+}
---
+++ naveDb/Tables/MiseAJour.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE dbo.Evalutation ADD FK_Path int(20) NOT NULL,
+CONSTRAINT [FK_Evalutation_Path] FOREIGN KEY ([FK_Path]) REFERENCES [Path]([Id]);
